7005a475318bdc11a2bf5a7d3bfadc028c7f8460
MadhanBaskar <154123283+MadhanBaskar@users.noreply.github.com>
2025-05-16 22:03:53 +0530

    Update output.tf

--- infra/output.tf
+++ infra/output.tf
@@ -1,11 +1,1 @@
-output "cluster_name" {
-  value = module.ecs_cluster.name
-}
 
-output "app1_service_name" {
-  value = module.app1_service.ecs_service_name
-}
-
-output "app2_service_name" {
-  value = module.app2_service.ecs_service_name
-}
