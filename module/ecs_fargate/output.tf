c69a865ffd75b953ab7bf82c0fb11c0565b5177d
MadhanBaskar <154123283+MadhanBaskar@users.noreply.github.com>
2025-05-17 07:19:49 +0000

    ferdffwf

--- module/ecs_fargate/output.tf
+++ module/ecs_fargate/output.tf
@@ -2,12 +2,12 @@ output "ecs_cluster_id" {
   value = aws_ecs_cluster.this.id
 }
 
-output "ecs_service1_arn" {
-  value = aws_ecs_service.this["service1"].id
+output "ecs_service_names" {
+  value = [for svc in values(aws_ecs_service.this) : svc.name]
 }
 
-output "ecs_service1_name" {
-  value = aws_ecs_service.this["service1"].name
+output "ecs_service_arns" {
+  value = [for svc in values(aws_ecs_service.this) : svc.id]
 }
 output "ecs_task_definition_arn" {
   value = aws_ecs_task_definition.this.arn
